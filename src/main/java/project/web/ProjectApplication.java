7ffa4529b30aac1cf4c868cef8fc4aebe002147b
alicatron <alison.ronayne@gmail.com>
2018-04-18 16:41:02 +0100

    Update1

--- src/main/java/project/web/ProjectApplication.java
+++ src/main/java/project/web/ProjectApplication.java
@@ -1,12 +1,28 @@
 package project.web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
+@ComponentScan(basePackages = "project")
+@EnableAutoConfiguration
 @SpringBootApplication
-public class ProjectApplication {
 
+
+public class ProjectApplication extends SpringBootServletInitializer {
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ProjectApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 }
+
